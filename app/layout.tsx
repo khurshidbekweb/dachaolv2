83376190417563265a775c0d2078f24834b0b2a0
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2024-10-18 20:57:10 +0500

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
 		type: 'website',
 		url: 'https://dachaol.uz',
 		locale: 'en_EN',
-		images: '../assets/DachaOL.uz.png',
+		images: '../assets/SeoImg.png',
 		countryName: 'Uzbekistan',
 		siteName: 'DachaOL',
 		emails: 'khurshidbeknuriddinov@gmail.com',
