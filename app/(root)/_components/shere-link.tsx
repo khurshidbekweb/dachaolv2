05e886c6b81c3728932f7c76e8460c1ba6482568
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2024-10-22 23:51:19 +0500

    update share

--- app/(root)/_components/shere-link.tsx
+++ app/(root)/_components/shere-link.tsx
@@ -2,14 +2,28 @@ import { Button } from '@/components/ui/button';
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Copy, Share2 } from 'lucide-react';
-import React from 'react';
+import { IMG_BASE_URL } from '@/constants/server';
+import { Check, Copy, Share2 } from 'lucide-react';
+import { usePathname } from 'next/navigation';
+import React, { useState } from 'react';
 
 const ShereLink = () => {
-    const copyLink = (e: HTMLFormElement) => {
+    const [copySuccess, setCopySuccess] = useState<string>('');
+    const pathname = usePathname()
+    console.log(pathname);
+    const link = 'https://dachaol.uz'+pathname     
+
+
+    const copyToClipboard = async (e) => {
         e.preventDefault()
-        
-    }
+        try {
+          await navigator.clipboard.writeText(e.target.link.value);
+          setCopySuccess('Link copied! You can now share it.');
+        } catch (err) {
+          setCopySuccess('Failed to copy the link');
+          console.error('Failed to copy: ', err);
+        }
+      };
     return (
         <Dialog>
             <DialogTrigger asChild>
@@ -25,25 +39,23 @@ const ShereLink = () => {
                         Anyone who has this link will be able to view this.
                     </DialogDescription>
                 </DialogHeader>
-                <div className="flex items-center space-x-2">
-                    <form>
+                <form onSubmit={copyToClipboard} className="flex items-center space-x-2">
                         <div className="grid flex-1 gap-2">
                             <Label htmlFor="link" className="sr-only">
                                 Link
                             </Label>
                             <Input
                                 id="link"
-                                defaultValue="https://ui.shadcn.com/docs/installation"
+                                defaultValue={link}
                                 readOnly
                                 name='link'
                             />
                         </div>
                         <Button type="submit" size="sm" className="px-3">
                             <span className="sr-only">Copy</span>
-                            <Copy className="h-4 w-4" />
+                            {copySuccess  ? <Check/> : <Copy className="h-4 w-4" />}
                         </Button>
-                    </form>
-                </div>
+                </form>
                 <DialogFooter className="sm:justify-start">
                     <DialogClose asChild>
                         <Button type="button" variant="secondary">
