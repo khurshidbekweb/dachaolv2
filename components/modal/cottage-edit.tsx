31a11b5f2e4b72f547186eb9cd87ba2ccb73a1b4
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2024-10-29 01:49:33 +0500

    start edit cottage

---
+++ components/modal/cottage-edit.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '../ui/dialog';
+
+const CottageEdit = () => {
+    return (
+        <Dialog>
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent>
+            <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+                This action cannot be undone. This will permanently delete your account
+                and remove your data from our servers.
+            </DialogDescription>
+            </DialogHeader>
+        </DialogContent>
+        </Dialog>
+
+    );
+};
+
+export default CottageEdit;
