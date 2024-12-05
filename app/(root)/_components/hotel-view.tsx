c3780493dcf9a43b864e785f985ba4399816fc63
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2024-12-05 14:26:24 +0500

    adds

--- app/(root)/_components/hotel-view.tsx
+++ app/(root)/_components/hotel-view.tsx
@@ -10,11 +10,19 @@ import { Keyboard, Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import HotelImg from '@/assets/mehmonhona.jpeg'
 import Sanatoriya from '@/assets/sanatoriya.jpg'
+import { ALL_DATA } from "@/Query/get_all";
+import { cottageType } from "@/types";
+import { IMG_BASE_URL } from "@/constants/server";
 
 
 const HotelView = () => {
+    const cottageType = ALL_DATA.useCottageType()?.data
     const { language } = useLanguageStore();
     const [swiperInstance, setSwiperInstance] = useState(null);
+    const hotetId = ['9aa6de2d-42be-4465-9b1d-5d43dd49e1a0','3e54eff7-8a26-443b-a302-066cbe8a05ff', '52b306ee-6a60-47b8-bf9a-f3de02ad7ea0']
+    const getData = cottageType?.filter((data: { id: string; }) => hotetId.includes(data.id))
+    console.log(getData);
+    
     return (
         <div className="relative my-2 px-2">
         <h2 className="places-top my-2 text-2xl font-createRound md:text-4xl">{hotelAndAanatorium[language]}</h2>
@@ -42,42 +50,20 @@ const HotelView = () => {
             }}
             modules={[Keyboard, Navigation]}
         >
-            <SwiperSlide className="">
-                        <Link href={'/'} className="w-[350px] cursor-pointer overflow-hidden h-[150px] rounded-md">
+            {getData?.length && getData.map((el) =>(
+                <SwiperSlide key={el.id} className="">
+                        <Link href={`/cottage-type/${el.id}`} className="w-[350px] cursor-pointer overflow-hidden h-[150px] rounded-md">
                             <Image
                             sizes="350px"
                             width={350}
                             height={100}
                             className="!w-full h-[110px]  md:h-[130px]  md:w-[400px] cursor-pointer rounded-md"
-                            src={HotelImg}
+                            src={`${IMG_BASE_URL}${el.image}`}
                             alt="nature"
                     />
                 </Link>
             </SwiperSlide>
-            <SwiperSlide className="">
-                        <Link href={'/'} className="w-[350px] cursor-pointer overflow-hidden h-[150px] rounded-md">
-                            <Image
-                            sizes="(min-width: 350px)"
-                            width={350}
-                            height={100}
-                            className="!w-full h-[110px]  md:h-[130px]  md:w-[400px] cursor-pointer rounded-md"
-                            src={Sanatoriya}
-                            alt="nature"
-                    />
-                </Link>
-            </SwiperSlide>
-            <SwiperSlide className="">
-                        <Link href={'/'} className="w-[350px] md:-w-[450px] cursor-pointer overflow-hidden h-[150px] rounded-md">
-                            <Image
-                            sizes="(min-width: 350px)"
-                                width={350}
-                                height={100}
-                                className="!w-full h-[110px] md:h-[130px] md:w-[400px] cursor-pointer rounded-md"
-                                src={HotelImg}
-                                alt="nature"
-                            />
-                        </Link>
-            </SwiperSlide>
+            ))}
         </Swiper>
         <button
             onClick={() => swiperInstance?.slidePrev()} // Swiper instance orqali slidePrev
--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -13,14 +13,13 @@ const Contact = () => {
         <div className="max-w-6xl mx-auto px-3 md:px-1">
             <div className="min-h-[20vh] flex flex-col justify-end items-start">
                 <BreacdCrambs data={[{slug: '', title:'Home'}]} page="Contact"/>
-                <h2 className='text-2xl md:text-3xl font-createRound'>Contact us</h2>
+                <h2 className='text-2xl md:text-3xl font-createRound'>Biz bilan bog`lanish</h2>
             </div>
             <div className='grid grid-cols-2 max-md:grid-cols-1 gap-4 mt-6'>
 				<div className='flex flex-col'>
-					<h1 className='text-4xl font-creteRound'>Contact DachaOL</h1>
+					<h1 className='text-4xl font-creteRound'>DachaOL bilan bog`laning</h1>
 					<p className='mt-2 text-muted-foreground'>
-						I am here to help and answer any question you might have. I look
-						forward to hearing from you
+					Men sizga yordam berish va har qanday savolga javob berish uchun shu yerdaman. Sizdan xabar kutib qolaman
 					</p>
 
 					<div className='mt-12 flex items-center gap-3'>
@@ -34,7 +33,7 @@ const Contact = () => {
 				</div>
 
 				<div>
-					<h1 className='text-4xl font-creteRound mb-2'>Contact form</h1>
+					<h1 className='text-4xl font-creteRound mb-2'>Bog`lanish</h1>
 					<div className='flex flex-col space-y-3'>
 						<ContactForm/>
 					</div>
