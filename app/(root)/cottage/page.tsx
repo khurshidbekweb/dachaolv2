e703f86f6a5eb28eff89fcbe207b9e7cb7c240a8
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2025-03-07 00:33:30 +0500

    add not foun

--- app/(root)/cottage/page.tsx
+++ app/(root)/cottage/page.tsx
@@ -16,11 +16,11 @@ import { cottageUtils } from "@/utils/cottage.utils";
 import { useQuery } from "@tanstack/react-query";
 import Loading from "@/components/loading/loading";
 import { comfort, cottage, cottageType, place, region } from "@/types";
+import { ChevronDown } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 
 const CottageAll = () => {
     const { t } = useTranslation();
-    const store = useLanguageStore();
-    const language = store.language;
     
     const [value, setValue] = useState(12000000);
     const [selectedRegion, setSelectedRegion] = useState("");
@@ -35,9 +35,10 @@ const CottageAll = () => {
     const comforts = ALL_DATA.useComforts()?.data;
     const cottageType = ['c23f5e39-c51e-4135-9c45-9142924b008f', 'c4c301b1-4719-499e-bde2-2c36715fae9e', '3e54eff7-8a26-443b-a302-066cbe8a05ff']
     const cottageTypeSelectChart = cottageTypes?.filter((el:cottageType) => cottageType.includes(el.id))
-    const [moreInfo, setMoreInfo] = useState(false)
+    const [moreInfo, setMoreInfo] = useState(5)
+    const isAllShown = moreInfo >= comforts?.length
     const [filter, setFilter] = useState({  
-        minPrice: 10,
+        minPrice: 500000,
         maxPrice: value,
         placeId: "",
         regionId: "",
@@ -47,7 +48,7 @@ const CottageAll = () => {
 
     useEffect(() => {
         setFilter({
-            minPrice: 10,
+            minPrice: 500000,
             maxPrice: value,
             placeId: selectedPlace || "",
             regionId: selectedRegion || "",
@@ -81,7 +82,7 @@ const CottageAll = () => {
                         </div>
                         <Slider
                             defaultValue={[value]}
-                            min={600000}
+                            min={500000}
                             max={12000000}
                             step={1}
                             onValueChange={(val) => setValue(val[0])}
@@ -108,6 +109,7 @@ const CottageAll = () => {
                             ))}
                         </SelectContent>
                     </Select>
+                    <Separator className="my-2"/>
                     <div>
                         <p className="py-2 font-medium">{t('dacha_type')}</p>
                         {cottageTypeSelectChart?.map((e:cottageType) => (
@@ -118,9 +120,10 @@ const CottageAll = () => {
                                 <p>{e.name}</p>
                             </label>
                         ))}
-                        <p className="py-2 font-medium">{t('camforts')}</p>
+                        <Separator className="my-2"/>
+                        <p className="py-1 font-medium">{t('camforts')}</p>
                         <div className="flex flex-col space-y-1">
-                            {comforts?.map((e:comfort) => (
+                            {comforts?.slice(0, moreInfo)?.map((e:comfort) => (
                                 <label key={e.id} className="flex items-center gap-2">
                                     <input className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" type="checkbox" value={e.id} onChange={(ev) => {
                                         setSelectedComforts(prev => ev.target.checked ? [...prev, e.id] : prev.filter(id => id !== e.id));
@@ -129,11 +132,25 @@ const CottageAll = () => {
                                     <p className="line-clamp-1">{e.name}</p>
                                 </label>
                             ))}
+                            {!isAllShown && (
+                                <button className="flex gap-x-1 justify-center items-center" onClick={() => setMoreInfo(comforts.length)}>Ko`proq ko`rish <ChevronDown/></button>
+                            )}
                         </div>
                     </div>
                 </div>
                 <div className="w-full grid grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-2 md:gap-4">
-                    {isLoading ? <Loading /> : (filteredCottages || cottages)?.filter(((dacha:cottage) => dacha.cottageType[0].id === "c4c301b1-4719-499e-bde2-2c36715fae9e"))?.map((dacha:cottage) => <Dacha key={dacha.id} {...dacha} />)}
+                {isLoading ? (
+                        <Loading />
+                    ) : (filteredCottages || cottages)?.length === 0 ? <>
+                        <p className="col-span-full text-center text-red-500">Ma’lumot topilmadi</p>                        
+                        </> : (
+                        (filteredCottages || cottages)
+                        ?.filter(
+                            (dacha: cottage) =>
+                            dacha.cottageType[0].id !== "678811f1-d67c-42fa-a78c-f1d980df3397"
+                        )
+                        ?.map((dacha: cottage) => <Dacha key={dacha.id} {...dacha} />)
+                    )}
                 </div>
             </div>
         </div>
