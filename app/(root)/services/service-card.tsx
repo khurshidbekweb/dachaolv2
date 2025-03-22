a0963efad70db79fe00bdcf9aec1a69e5c51fe0f
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2025-03-22 10:50:32 +0500

    add fixed

--- app/(root)/services/service-card.tsx
+++ app/(root)/services/service-card.tsx
@@ -10,17 +10,17 @@ const ServiceCard = (props: services) => {
     const language = store.language as keyof langKey
     
     return (
-        <div className="services-card relative w-full md:max-w-[450px] h-[400px] rounded-md overflow-hidden shadow-lg border flex flex-col items-start justify-start">
+        <div className="services-card relative w-full md:max-w-[400px] h-[400px] rounded-md overflow-hidden shadow-lg border flex flex-col items-start justify-start">
             <Image
                 src={`${IMG_BASE_URL}${props.images[0]}`}
                 alt={props.name}
-                className="w-[150px] mx-auto object-cover"
+                className="w-[200px] mx-auto object-cover p-1"
                 sizes="(min-width: 150px)"
                 width={150}
                 height={100}
             />
             <hr />
-            <div className="px-2">
+            <div className="p-3">
                 <h4 className="text-[18px] font-medium capitalize">{props.name}</h4>
                 <p className="services-card-description">{props.description}</p>
                 <Link className="bg-secondary text-black w-[95%] font-medium text-center absolute bottom-2 dark:text-white p-[6px] px-5 rounded-md" href={`/services/${props.id}`}>
