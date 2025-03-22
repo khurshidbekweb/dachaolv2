a134ff1940f924f7f1dce7cdd06de517c46d918f
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2025-03-22 09:34:18 +0500

    fixed mini problem

--- app/(root)/services/service-card.tsx
+++ app/(root)/services/service-card.tsx
@@ -10,18 +10,18 @@ const ServiceCard = (props: services) => {
     const language = store.language as keyof langKey
     
     return (
-        <div className="services-card relative w-full md:max-w-[300px] h-[350px] rounded-md overflow-hidden shadow-lg border flex flex-col items-start justify-start">
+        <div className="services-card relative w-full md:max-w-[450px] h-[400px] rounded-md overflow-hidden shadow-lg border flex flex-col items-start justify-start">
             <Image
                 src={`${IMG_BASE_URL}${props.images[0]}`}
                 alt={props.name}
-                className="w-full h-[180px]"
+                className="w-[150px] mx-auto object-cover"
                 sizes="(min-width: 150px)"
                 width={150}
                 height={100}
             />
             <hr />
             <div className="px-2">
-                <h4 className="text-xl font-medium capitalize">{props.name}</h4>
+                <h4 className="text-[18px] font-medium capitalize">{props.name}</h4>
                 <p className="services-card-description">{props.description}</p>
                 <Link className="bg-secondary text-black w-[95%] font-medium text-center absolute bottom-2 dark:text-white p-[6px] px-5 rounded-md" href={`/services/${props.id}`}>
                     {ServicesPageLanguage.viewTariff[language]}
